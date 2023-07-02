918febc3e8d9744042f37f65f5e741fd205fb091
Azikenp <azikenp26@gmail.com>
2023-07-02 23:21:07 +0100

    new

--- app.js
+++ app.js
@@ -6,18 +6,43 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 
-const getCountryData = function(country){
+// const getCountryData = function(country){
 
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//     request.send();
 
-    request.addEventListener('load', function(){
-        const [data] = JSON.parse(this.responseText)
-        console.log(data);
-        // console.log(Object.values(data.languages));
+//     request.addEventListener('load', function(){
+//         const [data] = JSON.parse(this.responseText)
+//         console.log(data);
+//         // console.log(Object.values(data.languages));
 
-        const html = `
+//         const html = `
+//         <article class="country">
+//             <img class="country__img" src="${data.flags.svg}" />
+//             <div class="country__data">
+//                 <h3 class="country__name">${data.name.common}</h3>
+//                 <h4 class="country__region">${data.region}</h4>
+//                 <p class="country__row"><span>👫</span>${(Number(data.population) / 1000000).toFixed(1) }  people</p>
+//                 <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
+//                 <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+//             </div>
+//         </article>
+//         `;
+
+//         countriesContainer.insertAdjacentHTML('beforeend', html);
+//         countriesContainer.style.opacity = 1;
+//     })
+// };
+
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('germany');
+
+
+
+const renderCountry = function(data){
+    const html = `
         <article class="country">
             <img class="country__img" src="${data.flags.svg}" />
             <div class="country__data">
@@ -32,9 +57,40 @@ const getCountryData = function(country){
 
         countriesContainer.insertAdjacentHTML('beforeend', html);
         countriesContainer.style.opacity = 1;
-    })
+}
+
+const getCountryAndNeighbour = function(country){
+
+    //AJAX call country 1
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', function(){
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        // console.log(Object.values(data.languages));
+
+        //Render country 1
+        renderCountry(data)
+
+        //Get neighbour country 2
+        const [neighbour] = data.borders;
+
+        if(!neighbour) return;
+
+         //AJAX call country 2
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+        request2.send();
+        request2.addEventListener('load', function(){
+            const [data2] = JSON.parse(this.responseText);
+            console.log(data2);
+            renderCountry(data2)
+        })
+
+    });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
+getCountryAndNeighbour('portugal');
 

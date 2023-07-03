b4d2fe059df15340a8412e9cd9cba71a78685dcd
Azikenp <azikenp26@gmail.com>
2023-07-03 21:32:37 +0100

    handlimg errors

--- app.js
+++ app.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+
+
+const renderError = function(msg){
+    countriesContainer.insertAdjacentText('beforeend' ,msg);
+}
+
+
+const renderCountry = function(data, className = ''){
+    const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flags.svg}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(Number(data.population) / 1000000).toFixed(1) } million  people</p>
+                <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
+                <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+            </div>
+        </article>
+        `;
+
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+}
+
 ///////////////////////////////////////
 
 
@@ -41,23 +65,6 @@ const countriesContainer = document.querySelector('.countries');
 
 
 
-const renderCountry = function(data, className = ''){
-    const html = `
-        <article class="country ${className}">
-            <img class="country__img" src="${data.flags.svg}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name.common}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(Number(data.population) / 1000000).toFixed(1) } million  people</p>
-                <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
-                <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
-            </div>
-        </article>
-        `;
-
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
-}
 
 
 
@@ -107,6 +114,7 @@ const renderCountry = function(data, className = ''){
 
 
 /////////////////////////////////////////////////////////////// Using Promises
+
 const getCountryData = function(country){
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
@@ -125,12 +133,18 @@ const getCountryData = function(country){
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         renderCountry(data[0], 'neighbour')
+    })
+    .catch(err => {
+        console.error(`${err} haha`);
+        renderError(`something went wrong. . . . ${err.message}`);
+    })
+    .finally( () => {
+        countriesContainer.style.opacity = 1;
     })
 };
 
-getCountryData('usa');
+getCountryData('brazil');
 
 
 

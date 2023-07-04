58610f0ed33664203d67201f48f9bd71c21ca0e5
Azikenp <azikenp26@gmail.com>
2023-07-05 00:15:13 +0100

    promises ended

--- app.js
+++ app.js
@@ -196,14 +196,31 @@ const timeout = function(sec){
     return new Promise(function(_, reject){
         setTimeout(function(){
             reject (new Error('Request took too long'));
-        }, sec)
+        }, sec * 1000)
     });
 };
 
 
 Promise.race([
-    getJSON(`https://restcountries.com/v3.1/name/tanzania`),timeout(1)
-])
+    getJSON(`https://restcountries.com/v3.1/name/tanzania`),timeout(5)
+]).then(res => console.log(res[0])).catch(err => console.error(err));
+
+
+//Promise.allSettled
+Promise.all([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another Success'),
+]).then(res => console.log(res)).catch(err => console.error(err));
+
+
+
+//Promise.any [ES2021]
+Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another Success'),
+]).then(res => console.log(res)).catch(err => console.error(err));
 
 
 
